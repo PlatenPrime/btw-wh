ef6dc5ed7789fc83ca7d39c0793ae045d28bdebc
Aleksandr Marchenko <77555146+PlatenPrime@users.noreply.github.com>
2025-02-08 15:20:40 +0200

    Update axios.ts

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-	baseURL: 'https://btw-server.up.railway.app/api',
+	baseURL: 'https://btw-wh.up.railway.app/api',
 	// baseURL: 'http://localhost:3002/api/',
 })
 
