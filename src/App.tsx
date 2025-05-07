c0c752cf4b6cb04084cba79ecd2cb8aef647c884
Aleksandr Marchenko <proshta@gmail.com>
2025-05-07 14:01:03 +0300

    dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
 import "./App.css";
+import { ModeToggle } from "./components/mode-toggle";
+import { ThemeProvider } from "./components/theme-provider";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
-    <>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <h1 className="text-3xl font-bold underline bg-background text-foreground">
         Hello world!
       </h1>
+      <Button variant="outline">Button</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="destructive">Destructive</Button>
+      <ModeToggle />
       <div
         className="bg-zinc-950 dark:bg-white size-8
       "
       />
       <div className="bg-primary text-primary-foreground ">Hello</div>
-    </>
+    </ThemeProvider>
   );
 }
 
