3a902a6aaeaa6e356af261d68d4e536396522647
Aleksandr Marchenko <proshta@gmail.com>
2025-05-07 12:56:18 +0300

    new start from the ground

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline bg-background text-foreground">
+        Hello world!
+      </h1>
+      <div
+        className="bg-zinc-950 dark:bg-white size-8
+      "
+      />
+      <div className="bg-primary text-primary-foreground ">Hello</div>
+    </>
+  );
+}
+
+export default App;
