c9fdd31312c75897e75179abe4b0cdae237eb39f
Aleksandr Marchenko <77555146+PlatenPrime@users.noreply.github.com>
2025-05-29 08:27:28 +0300

    attempt to solve 404 problem #3432524424

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
+import { Loader } from "./components/loader.tsx";
 import "./index.css";
 import { router } from "./router.tsx";
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { Loader } from "./components/loader.tsx";
 
 const queryClient = new QueryClient();
 
@@ -13,7 +13,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <Suspense fallback={<Loader />}>
-      <RouterProvider router={router} />
+        <RouterProvider router={router} />
       </Suspense>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
