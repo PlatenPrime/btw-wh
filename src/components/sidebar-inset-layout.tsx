b4970914859502c804a886ce876e18abc5186b57
Aleksandr Marchenko <77555146+PlatenPrime@users.noreply.github.com>
2025-06-07 08:09:36 +0300

    test some acernity and cute header

--- src/components/sidebar-inset-layout.tsx
+++ src/components/sidebar-inset-layout.tsx
@@ -1,23 +1,38 @@
+import React from "react";
 import { Separator } from "./ui/separator";
 import { SidebarInset, SidebarTrigger } from "./ui/sidebar";
+import { Vortex } from "./ui/vortex";
 
 export function SidebarInsetLayout({
   headerText,
   children,
 }: {
   headerText?: string;
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
   return (
-    <SidebarInset>
-      <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-        <SidebarTrigger className="-ml-1" />
-        <Separator orientation="vertical" className="mr-2 h-4" />
-        {headerText && (
-          <h1 className="text-2xl font-bold">{headerText}</h1>
-        )}
+    <SidebarInset className="relative">
+      {/* Header с фоновым Vortex */}
+
+      <header className="relative flex h-16 shrink-0 items-center gap-2 border-b px-4 overflow-hidden">
+        <div className="absolute inset-0 overflow-hidden blur">
+          <Vortex
+            backgroundColor="black"
+            rangeY={800}
+            particleCount={500}
+            baseHue={210}
+            className="absolute inset-0 -z-10 pointer-events-none"
+          ></Vortex>
+        </div>
+        <div className="flex items-center relative z-10 inset-0">
+          <SidebarTrigger className="-ml-1" />
+          <Separator orientation="vertical" className="mr-2 h-4" />
+          {headerText && <h1 className="text-2xl font-bold">{headerText}</h1>}
+        </div>
       </header>
-      {children}
-    </SidebarInset> 
+
+      <div className="relative z-20">{children}</div>
+      {/* Контент после хедера — должен быть поверх всего */}
+    </SidebarInset>
   );
 }
--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -3,7 +3,7 @@ import { SidebarInsetLayout } from "@/components/sidebar-inset-layout";
 export function Main() {
   return (
     <SidebarInsetLayout headerText="Головна">
-      <main className=" p-4"></main>
+      <main className=""></main>
     </SidebarInsetLayout>
   );
 }
