68625c7ec71104c822edf05ba28856adfd28ffe3
Aleksandr Marchenko <proshta@gmail.com>
2025-06-20 15:29:41 +0300

    cool background effect in img card

--- src/modules/arts/components/dashboard-grid-card/index.tsx
+++ src/modules/arts/components/dashboard-grid-card/index.tsx
@@ -1,7 +1,7 @@
 import { Image } from "@/components/image";
 import { Card, CardContent, CardDescription } from "@/components/ui/card";
 import { Link } from "react-router";
-import { getBigImageUrl } from "../../services/art-image-url";
+import { getSmallImageUrl } from "../../services/art-image-url";
 import type { ArtDto } from "../../types/dto";
 
 interface GridCardProps {
@@ -11,17 +11,26 @@ interface GridCardProps {
 export function GridCard({ art }: GridCardProps) {
   return (
     <Link to={`/arts/${art.artikul}`} className="block h-full w-full ">
-      <Card className="flex h-full flex-col justify-between hover:scale-102 hover:shadow-2xl hover:shadow-gray-500 transition-all duration-300">
+      <Card className="flex h-full relative flex-col justify-between hover:scale-102 hover:shadow-2xl hover:shadow-gray-500 transition-all duration-300 p-2">
         {/* <CardHeader className="text-center">
           <CardTitle>{art.artikul}</CardTitle>
         </CardHeader> */}
 
-        <CardContent className="flex justify-center">
+        <div className="bg-red-500/10 absolute top-0 overflow-hidden flex justify-center blur opacity-10">
+          {" "}
           <Image
-            src={getBigImageUrl(art.artikul)}
+            src={getSmallImageUrl(art.artikul)}
             alt={art.nameukr}
             // квадратное превью без «прыжков» сетки
-            className="aspect-square w-full max-w-[10rem] object-cover rounded-md"
+            className="aspect-square w-[24rem]  object-cover rounded-md"
+          />
+        </div>
+        <CardContent className="flex justify-center">
+          <Image
+            src={getSmallImageUrl(art.artikul)}
+            alt={art.nameukr}
+            // квадратное превью без «прыжков» сетки
+            className="aspect-square w-full max-w-[6rem] object-cover rounded-md"
           />
         </CardContent>
 
--- src/modules/arts/components/dashboard-grid-card/skeleton.tsx
+++ src/modules/arts/components/dashboard-grid-card/skeleton.tsx
@@ -1,30 +1,24 @@
+import { Card, CardContent, CardDescription } from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 
 export function GridCardSkeleton() {
   return (
     <div className="block h-full w-full">
-    <Card className="flex h-full flex-col justify-between">
-      <CardHeader className="text-center">
+      <Card className="flex h-full flex-col justify-between">
+        {/* <CardHeader className="text-center">
         <CardTitle>
           <Skeleton className="h-6 w-24 mx-auto" />
         </CardTitle>
-      </CardHeader>
+      </CardHeader> */}
 
-      <CardContent className="flex justify-center">
-        <Skeleton className="aspect-square w-full max-w-[10rem] rounded-md" />
-      </CardContent>
+        <CardContent className="flex justify-center">
+          <Skeleton className="aspect-square w-full max-w-[4rem] rounded-md" />
+        </CardContent>
 
-      <CardDescription className="p-2 text-center">
-        <Skeleton className="h-4 w-32 mx-auto" />
-      </CardDescription>
-    </Card>
+        <CardDescription className="p-2 text-center">
+          <Skeleton className="h-4 w-32 mx-auto" />
+        </CardDescription>
+      </Card>
     </div>
   );
 }
