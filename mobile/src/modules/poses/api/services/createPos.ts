9e4626e6c568777fe0d5a3fe5f94dab536a77abb
Aleksandr Marchenko <proshta@gmail.com>
2025-10-20 11:47:32 +0300

    clear mobile again

--- mobile/src/modules/poses/api/services/createPos.ts
+++
@@ -1,7 +0,0 @@
-import { apiClient } from '../../../../lib/apiClient';
-import type { CreatePosDto, PosResponse } from '../types/index';
-
-export const createPos = async (data: CreatePosDto, signal?: AbortSignal): Promise<PosResponse> => {
-  const res = await apiClient.post<PosResponse>('/poses', data, { signal });
-  return res.data;
-};
--- mobile/src/modules/poses/api/services/deletePos.ts
+++
@@ -1,5 +0,0 @@
-import { apiClient } from '../../../../lib/apiClient';
-
-export const deletePos = async (id: string, signal?: AbortSignal): Promise<void> => {
-  await apiClient.delete(`/poses/${id}`, { signal });
-};
